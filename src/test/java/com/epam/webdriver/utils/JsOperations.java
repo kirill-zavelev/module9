85ccdc3c3adb10e21f797d120f78d43d795d2a24
ilyamakhon <ilyamakhon9135@gmail.com>
2020-05-27 18:46:10 +0300

    jsutils  simplified

--- src/test/java/com/epam/webdriver/utils/JsOperations.java
+++ src/test/java/com/epam/webdriver/utils/JsOperations.java
@@ -8,9 +8,9 @@ import org.openqa.selenium.WebElement;
 public class JsOperations {
 
     private static final WebDriver DRIVER = DriverSingleton.getDriver().getWebDriver();
+    private static final String JS_SCRIPT = "arguments[0].setAttribute('style', 'background: yellow; border: 2px solid red;');";
 
     public static void highLightText(WebElement element) {
-        ((JavascriptExecutor) DRIVER).executeScript
-                ("arguments[0].setAttribute('style', 'background: yellow; border: 2px solid red;');", element);
+        ((JavascriptExecutor) DRIVER).executeScript(JS_SCRIPT, element);
     }
 }
