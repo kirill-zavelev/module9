d881ec97a7c51a1a8cbf6b5b8bc0fd0b41ce6ee1
ilyamakhon <ilyamakhon9135@gmail.com>
2020-05-27 18:38:15 +0300

    modifcators modified

--- src/test/java/com/epam/webdriver/base/BaseTest.java
+++ src/test/java/com/epam/webdriver/base/BaseTest.java
@@ -2,23 +2,25 @@ package com.epam.webdriver.base;
 
 import com.epam.webdriver.decorator.DriverDecorator;
 import com.epam.webdriver.driver.DriverSingleton;
+import com.epam.webdriver.factory.EmailFactory;
 import com.epam.webdriver.page.auth.LoginPage;
 import com.epam.webdriver.page.auth.QuickActionsPanelPage;
 import com.epam.webdriver.page.mailactions.MailCreationPage;
 import com.epam.webdriver.page.mailfolders.DraftPage;
 import com.epam.webdriver.page.mailfolders.InboxPage;
-import com.epam.webdriver.factory.EmailFactory;
 import com.epam.webdriver.utils.PropertyLoader;
 import com.epam.webdriver.utils.TestListener;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
 @Listeners({TestListener.class})
 public class BaseTest {
 
-    protected static final String USERNAME = PropertyLoader.loadProperty("user.name");
-    protected static final String PASSWORD = PropertyLoader.loadProperty("user.password");
+    private static final String USERNAME = PropertyLoader.loadProperty("user.name");
+    private static final String PASSWORD = PropertyLoader.loadProperty("user.password");
     protected static final String EMAIL = PropertyLoader.loadProperty("user.send.from");
-    protected static final String BASE_URL = PropertyLoader.loadProperty("base.url");
+    private static final String BASE_URL = PropertyLoader.loadProperty("base.url");
     protected static final EmailFactory EMAIL_FACTORY = new EmailFactory();
 
     protected LoginPage loginPage;
